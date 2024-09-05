d6dfd6153c9ed4c219356e9bd463e7c42679f369
Kurt metzelfeld <kmetz73@gmail.com>
2024-09-05 16:23:42 -0500

    code  with  error on action file

--- utils/actions.ts
+++ utils/actions.ts
@@ -13,14 +13,28 @@ export const createProfileAction = async (
 ) => {
   try {
     const user = await currentUser();
-    console.log(user);
+    if (!user) throw new Error('Please login to create a profile');
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: 'Profile created' };
-  } catch (error) {
-    console.log(error);
 
-    return { message: 'There was an error' };
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
   }
+  redirect('/');
 };
